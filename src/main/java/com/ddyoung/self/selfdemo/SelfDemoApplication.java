f5fa54d9c5aa27196994d04a62554858e59eb364
sanbian <sanbian@shulie.io>
2018-12-05 14:24:16 +0800

    init

---
+++ src/main/java/com/ddyoung/self/selfdemo/SelfDemoApplication.java
@@ -0,0 +1,12 @@
+package com.ddyoung.self.selfdomo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfDomoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfDomoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ddyoung/self/selfdemo/SelfDomoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ddyoung.self.selfdomo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SelfDomoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
